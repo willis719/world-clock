d57aa98b21d8e40308bbda9d0f2ab4d45212c997
Willie Rose <willierose87@gmail.com>
2021-04-01 15:54:53 -0500

    Gave the user the ability to delete saved timezones

--- src/pages/MyTimezones.js
+++ src/pages/MyTimezones.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Clock from '../components/Clock'
 
 export default function MyTimezones(props) {
+
+    const removeTimezone = (timezone) => {
+        props.setTimezones(props.timezones.filter((tz => {
+            return timezone.value !== tz.value
+        }))
+        )
+    }
+
     return (
         <div>
             {props.timezones.map(tz => {
@@ -9,6 +17,7 @@ export default function MyTimezones(props) {
                     <div>
                         <h2>{tz.value}</h2>
                         <Clock timezone={tz} />
+                        <button onClick={() => removeTimezone(tz)}>Remove</button>
                     </div>  
                 )
             })}
